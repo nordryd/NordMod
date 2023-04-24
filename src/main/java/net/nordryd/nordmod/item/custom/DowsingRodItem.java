ab8f7c57d21d90fcd3c01b22bd403ada159b681d
BuildTools <unconfigured@null.spigotmc.org>
2023-04-24 18:40:02 -0500

    Changed valuable found message to use localication instead of registry name

--- src/main/java/net/nordryd/nordmod/item/custom/DowsingRodItem.java
+++ src/main/java/net/nordryd/nordmod/item/custom/DowsingRodItem.java
@@ -7,7 +7,6 @@ import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -25,46 +24,41 @@ public class DowsingRodItem extends ModItem
     @Override
     public InteractionResult useOn(final UseOnContext useOnContext) {
         // isClientSide ensures that this executed on the client side (we don't wanna broadcast this to the whole server)
-        if(useOnContext.getLevel().isClientSide()) {
+        if (useOnContext.getLevel().isClientSide()) {
             final BlockPos positionClicked = useOnContext.getClickedPos();
             final Player player = useOnContext.getPlayer();
+
+            final int blocksBelowToWorldBottom = positionClicked.getY() + 64;
+            int blocksBelow = 0;
             boolean foundBlock = false;
 
-            // can we add sounds to this to add flair?
+            while (!foundBlock && blocksBelow <= blocksBelowToWorldBottom) {
+                final Block blockBelow = useOnContext.getLevel().getBlockState(positionClicked.below(blocksBelow)).getBlock();
 
-            // From positionClicked - 0 to positionClicked - (positionClicked + 64)
-            // This will iterate all the way down to Y = 0, and then 64 more blocks down because there's 64 more blocks below
-            // Y = 0 as of 1.18.
-            // change to a while loop to be a little cleaner
-            for(int blockY = 0; blockY <= positionClicked.getY() + 64; blockY++) {
-                final Block blockBelow = useOnContext.getLevel().getBlockState(positionClicked.below(blockY)).getBlock();
-
-                if(isValuableBlock(blockBelow)) {
-                    outputValuableCoordinates(positionClicked.below(blockY), player, blockBelow);
+                if (isValuableBlock(blockBelow)) {
+                    outputValuableCoordinates(positionClicked.below(blocksBelow), player, blockBelow);
                     foundBlock = true;
-                    break;
+                }
+                else {
+                    blocksBelow++;
                 }
             }
 
-            // TODO use foundBlock as the while loop conditional to make this a bit cleaner
-
-            if(!foundBlock) {
-                player.sendMessage(new TranslatableComponent("item.nordmod.dowsing_rod.no_valuables"),
-                        player.getUUID());
+            if (!foundBlock) {
+                player.sendMessage(new TranslatableComponent("item.nordmod.dowsing_rod.no_valuables"), player.getUUID());
             }
         }
 
-        // Damage the item the player is holding by 1. If broken, then we broadcast a break event for the item used.
-        useOnContext.getItemInHand().hurtAndBreak(1, useOnContext.getPlayer(),
-                (player) -> player.broadcastBreakEvent(player.getUsedItemHand()));
+        useOnContext.getItemInHand()
+                .hurtAndBreak(1, useOnContext.getPlayer(), (player) -> player.broadcastBreakEvent(player.getUsedItemHand()));
 
         return super.useOn(useOnContext);
     }
 
     private void outputValuableCoordinates(final BlockPos blockPos, final Player player, final Block blockBelow) {
         player.sendMessage(new TextComponent(
-                String.format("Found %s at (%d, %d, %d)", blockBelow.asItem().getRegistryName().toString(), blockPos.getX(),
-                        blockPos.getY(), blockPos.getZ())), player.getUUID());
+                String.format("There's %s at (%d, %d, %d)!", blockBelow.getName().getString(), blockPos.getX(), blockPos.getY(),
+                        blockPos.getZ())), player.getUUID());
     }
 
     private boolean isValuableBlock(final Block block) {
