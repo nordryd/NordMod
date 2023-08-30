4576ee1be8acac8ab543f3ace6892776355437b8
BuildTools <unconfigured@null.spigotmc.org>
2023-08-30 16:26:47 -0500

    tooltips added

--- src/main/java/net/nordryd/nordmod/block/custom/SoundBlock.java
+++ src/main/java/net/nordryd/nordmod/block/custom/SoundBlock.java
@@ -1,16 +1,23 @@
 package net.nordryd.nordmod.block.custom;
 
+import java.util.List;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
 
 public class SoundBlock extends Block
 {
@@ -43,6 +50,13 @@ public class SoundBlock extends Block
         }
 
         return super.use(pBlockState, pLevel, pBlockPos, pPlayer, pHand, pHitResult);
+    }
+
+    @Override
+    public void appendHoverText(final ItemStack pItemStack, @Nullable final BlockGetter pBlockGetter,
+            final List<Component> pTooltip, final TooltipFlag pAdvanced) {
+        pTooltip.add(Component.translatable("tooltip.nordmod.sound_block"));
+        super.appendHoverText(pItemStack, pBlockGetter, pTooltip, pAdvanced);
     }
 
     @Override
--- src/main/java/net/nordryd/nordmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/nordryd/nordmod/item/custom/MetalDetectorItem.java
@@ -2,16 +2,21 @@ package net.nordryd.nordmod.item.custom;
 
 import java.util.List;
 
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 
 public class MetalDetectorItem extends Item
 {
@@ -39,10 +44,22 @@ public class MetalDetectorItem extends Item
             }
         }
 
-        pContext.getItemInHand().hurtAndBreak(1, pContext.getPlayer(),
-                player -> player.broadcastBreakEvent(player.getUsedItemHand()));
+        pContext.getItemInHand()
+                .hurtAndBreak(1, pContext.getPlayer(), player -> player.broadcastBreakEvent(player.getUsedItemHand()));
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(final ItemStack pItemStack, @Nullable final Level pLevel,
+            final List<Component> pTooltip, final TooltipFlag pAdvanced) {
+        if (Screen.hasShiftDown()) {
+            pTooltip.add(Component.translatable("tooltip.nordmod.metal_detector.tooltip.shift"));
+        }
+        else {
+            pTooltip.add(Component.translatable("tooltip.nordmod.metal_detector.tooltip"));
+        }
+        super.appendHoverText(pItemStack, pLevel, pTooltip, pAdvanced);
     }
 
     private void outputNoValuableFound(final Player player) {
